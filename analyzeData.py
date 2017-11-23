6ff812860712bf3373ac652a5a419070671def2a
Marvin Krüger <marvin.krueger@yahoo.de>
2017-11-23 13:46:36 +0100

    Counted images for each car brand

--- analyzeData.py
+++ analyzeData.py
@@ -45,4 +45,14 @@ for image in labels_and_paths:
             possible_brands.append(brand)
     image.append(max(possible_brands, key=len))
 
-print(labels_and_paths)
+#Count the number of images for each brand
+brands_in_data = []
+brands_count = []
+for image in labels_and_paths:
+    if(image[2] not in brands_in_data):
+        brands_in_data.append(image[2])
+        brands_count.append(1)
+    else:
+        brands_count[brands_in_data.index(image[2])] += 1
+for brand in range(len(brands_in_data)):
+    print(brands_in_data[brand], brands_count[brand])
