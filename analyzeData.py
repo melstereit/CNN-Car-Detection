a63aae6ae559853bc29076cbfaf3838080b8cb29
Marvin Krüger <marvin.krueger@yahoo.de>
2017-11-23 13:38:21 +0100

    Divide data by car brands

--- analyzeData.py
+++ analyzeData.py
@@ -1,5 +1,6 @@
 import scipy.io as sio
-
+from urllib import request, parse
+from htmldom import htmldom
 
 def getLabelsAndPathsFromMatFile(path):
     mat_file = sio.loadmat(path)
@@ -9,9 +10,39 @@ def getLabelsAndPathsFromMatFile(path):
     labels_and_paths = []
 
     for annotation in image_annos:
-        labels_and_paths.append((annotation[0][0], classes[annotation[5][0][0]-1][0]))
+        labels_and_paths.append([annotation[0][0], classes[annotation[5][0][0]-1][0]])
     return labels_and_paths
 
+def getAllCarBrands():
+    f = request.urlopen('https://www.kfz-mag.de/marken-modelle')
+    content = str(f.read(), 'utf-8')
+    html_dom = htmldom.HtmlDom().createDom(content)
+    brands = html_dom.find('.articleColumn .brand h3')
+    brands_array = []
+    for brand in brands:
+        brands_array.append(brand.text().lower())
+    brands_array.append("volkswagen")
+    brands_array.append("spyker")
+    brands_array.append("scion")
+    brands_array.append("plymouth")
+    brands_array.append("mclaren")
+    brands_array.append("geo")
+    brands_array.append("eagle")
+    brands_array.append("am general")
+    brands_array.append("hummer")
+
+
+    return brands_array
+
+brands = getAllCarBrands()
 labels_and_paths = getLabelsAndPathsFromMatFile('input/cars_annos.mat')
+
+#Just get the car brand and add in to each image in labels_and_paths
+for image in labels_and_paths:
+    possible_brands = []
+    for brand in brands:
+        if brand in image[1].lower():
+            possible_brands.append(brand)
+    image.append(max(possible_brands, key=len))
 
 print(labels_and_paths)
