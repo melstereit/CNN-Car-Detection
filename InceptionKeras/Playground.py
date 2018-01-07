0d32dafa73bed61be6c7f87b66f9b49b5b9a44d1
Marvin Krüger <s0556109@htw-berlinde>
2018-01-07 15:35:43 +0100

    Some refactoring. Added Xavier weight initialization for FC layers. Created a method to visualize what exactly the keras datagenerator does for debugging purposes.

--- InceptionKeras/Playground.py
+++ InceptionKeras/Playground.py
@@ -3,55 +3,54 @@ from keras.applications import InceptionV3
 from keras.applications.imagenet_utils import preprocess_input
 from keras.callbacks import LambdaCallback
 from keras.engine import Model
-from keras.layers import Dense, GlobalMaxPooling2D
-from keras.optimizers import SGD
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, GlobalMaxPooling2D, MaxPooling2D, Flatten, Dropout
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 import requests
 
 train_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_input,
-    rotation_range=30,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
+    preprocessing_function=preprocess_input
 )
 
 test_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_input,
-    rotation_range=30,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
+    preprocessing_function=preprocess_input
 )
 
 train_generator = train_datagen.flow_from_directory(
     "./data/train_data/images_per_class/",
     target_size=(299, 299),
-    batch_size=10
+    batch_size=10,
+    class_mode='categorical'
 )
+def save_preview():
+    # check out this
+    #https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+    #--> See which type of data the datagen will make out of the raw data
+    i = 0
+    for batch in train_datagen.flow_from_directory("./data/train_data/images_per_class/", target_size=(299, 299), batch_size=10,
+                                                   save_to_dir='preview', save_prefix='car', save_format='jpeg'):
+        i += 1
+        if i > 20:
+            break  # otherwise the generator would loop indefinitely
+
+
 
 validation_generator = test_datagen.flow_from_directory(
     "./data/val_data/images_per_class/",
     target_size=(299, 299),
-    batch_size=10
+    batch_size=10,
+    class_mode='categorical'
 )
 
 batch_counter = 0
 
-def send_mail_with_progress(batch, batch_counter):
-    batch_counter += 1
+def send_mail_with_text(text):
     print("Mail sent")
-    if batch_counter % 10 is 1:
-        content = "Model is in training. Computed" + batch_counter + " batches of 10 images."
-        req  = "https://api.elasticemail.com/v2/email/send?apikey=31abeb50-eff0-4863-a167-b7f1c8689c83&subject=Python info&from=meinemail@kruegerbiz.de&to=marvin.krueger@yahoo.de&msgTo=marvin.krueger@yahoo.de&bodyHtml=" + content + "&encodingType=0&isTransactional=True"
-        requests.get(req)
+    content = text
+    req  = "https://api.elasticemail.com/v2/email/send?apikey=31abeb50-eff0-4863-a167-b7f1c8689c83&subject=Python info&from=meinemail@kruegerbiz.de&to=marvin.krueger@yahoo.de&msgTo=marvin.krueger@yahoo.de&bodyHtml=" + content + "&encodingType=0&isTransactional=True"
+    requests.get(req)
 
-train_progress_mail_callback = LambdaCallback(
-    on_batch_begin=lambda batch,logs: send_mail_with_progress(batch, batch_counter))
+'''train_progress_mail_callback = LambdaCallback(
+    on_batch_begin=lambda batch,logs: send_mail_with_progress(batch))'''
 
 def initializing_and_train_top_layer():
     base_model = InceptionV3(weights='imagenet', include_top=False)
@@ -62,9 +61,11 @@ def initializing_and_train_top_layer():
 
     #Include top layer
     x = base_model.output
-    x = GlobalMaxPooling2D()(x)
-    x = Dense(1024, activation='relu')(x)
-    predictions = Dense(196, activation='softmax')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid')(x)
+    x = Dense(1024, activation='relu', kernel_initializer='glorot_normal', bias_initializer='zeros')(x)
+    x = Dense(500, activation='relu', kernel_initializer='glorot_normal', bias_initializer='zeros')(x)
+    predictions = Dense(196, activation='softmax', kernel_initializer='glorot_normal', bias_initializer='zeros')(x)
+
     model = Model(input=base_model.input, output=predictions)
 
     model.compile(optimizer='adam',
@@ -76,7 +77,6 @@ def initializing_and_train_top_layer():
         train_generator,
         samples_per_epoch=13096,
         nb_epoch=2,
-        callbacks=[train_progress_mail_callback],
         validation_data=validation_generator,
         nb_val_samples=3089,
         class_weight='auto')
@@ -85,6 +85,10 @@ def initializing_and_train_top_layer():
     model.save("model.keras")
 
 def train_top_layers():
+    # --> https://www.kaggle.com/abnera/transfer-learning-keras-xception-cnn
+    # If you want to train the last Two blocks of an Inception model it should be 172
+    # layers before this number will used the pre-trained weights, layers above and including this number
+    # will be re-trained based on the new data.
     layers_to_freeze = 172
     model = keras.models.load_model('model.keras')
     for layer in model.layers[:layers_to_freeze]:
@@ -99,13 +103,16 @@ def train_top_layers():
         train_generator,
         samples_per_epoch=13096,
         nb_epoch=2,
-        callbacks=[train_progress_mail_callback],
         validation_data=validation_generator,
         nb_val_samples=3089,
         class_weight='auto')
 
     #Save
     model.save("model.keras")
+    return history
+
 
 initializing_and_train_top_layer()
-train_top_layers()
+history_obj = train_top_layers()
+
+send_mail_with_text("Calculation completed! Accuracy: " + history_obj.history['acc'])
