dfc68d2d6f51d17eb2f7c61bcef765bd64f68a56
Hesels Christian <s0553310@htw-berlin.de>
2018-01-17 22:39:55 +0100

    Changed parameter for pool_sioze

--- CNNModel/carmodel.py
+++ CNNModel/carmodel.py
@@ -13,24 +13,24 @@ class Model:
     def _create_model(self, num_classes, input_shape):
         # Create the model
         model = Sequential()
-        model.add(Conv2D(96, 11, strides=4, border_mode='same'))
+        model.add(Conv2D(96, (11, 11), strides=4))
         model.add(BatchNormalization(()))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(poolsize=(5, 5)))
+        model.add(MaxPooling2D(pool_size=(5, 5)))
 
-        model.add(Conv2D(256, 5, border_mode='same'))
+        model.add(Conv2D(256, (5, 5)))
         model.add(BatchNormalization(()))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(poolsize=(3, 3)))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
 
-        model.add(Conv2D(384, 3, border_mode='same'))
+        model.add(Conv2D(384, (3, 3)))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(poolsize=(3, 3)))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
 
-        model.add(Conv2D(384, 3, border_mode='same'))
+        model.add(Conv2D(384, (3, 3)))
         model.add(Activation('relu'))
 
-        model.add(Conv2D(256, 3, border_mode='same'))
+        model.add(Conv2D(256, (3, 3)))
         model.add(Activation('relu'))
 
         model.add(Flatten())
